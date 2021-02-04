d9b2854c92f6f961d3b0290fd968e6301876bd17
Angelo <42555726+mormoops@users.noreply.github.com>
2021-02-03 22:42:30 -0500

    Update species_observations.R

--- data_mining/species_observations.R
+++ data_mining/species_observations.R
@@ -1,1 +1,6 @@
-
+###
+  ## mining & downloading spatial data: GBIF
+  # GBIF: simple query - 
+  # choose your favorite species and query the database
+pfal <- gbif("Phyllops", "falcatus", geo = FALSE)
+  # if geo = TRUE only records with lon/lat will be downloaded
