85f1def68ca1dd6495c84632c3683e4249c3255e
Angelo <42555726+mormoops@users.noreply.github.com>
2021-02-08 20:28:25 -0500

    Delete data_mining directory

--- data_mining/species_observations.R
+++
@@ -1,6 +0,0 @@
-###
-  ## mining & downloading spatial data: GBIF
-  # GBIF: simple query - 
-  # choose your favorite species to query the database
-sp <- gbif("GENUS", "SPECIES", geo = FALSE)
-  # if geo = TRUE only records with lon/lat will be downloaded
