54018d3943bb08e2f41b3ebb8b50a5bb5e1a0488
Angelo <42555726+mormoops@users.noreply.github.com>
2021-04-13 11:43:02 -0400

    Update 03_environ_data_mine.R

--- tutorial/03_environ_data_mine.R
+++ tutorial/03_environ_data_mine.R
@@ -1,2 +1,26 @@
 ###
-  ## mining & downloading environmental data: WorldClim (www.worldclim.org)
+  ## downloading & wrangling environmental data: WorldClim (www.worldclim.org)
+
+library(raster)
+
+  # NOTE: this will download, decompress, and make a raster stack of 2.5' resolution climate data 
+    # for specific arguments see ?getData
+    # recommended: create a specific directory where data will be downloaded & add it into path
+bioclim.data <- getData(name = "worldclim", var = "bio", res = 2.5, path = "~/")
+
+  # examine the downloaded raster files
+plot(bioclim.data$bio1)
+
+  # create a geographic extent for your study area
+    # change lon/lat coordinates
+geo.extent <- extent(-86, -65, 16, 26) # e.g. Greater Antilles
+
+  # crop bioclim predictors to the geographic extent
+bioclim.data <- crop(bioclim.data, geo.extent)
+
+  # examine cropped raster files
+plot(bioclim.data$bio1)
+
+
+### 
+  ## upload and wrangle already downloaded bioclim data predictors
