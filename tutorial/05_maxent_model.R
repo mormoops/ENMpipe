40fd52235440afaa55c474d3682ae016392dbbbc
Angelo <42555726+mormoops@users.noreply.github.com>
2021-06-28 11:26:57 -0400

    Update 05_maxent_model.R

--- tutorial/05_maxent_model.R
+++ tutorial/05_maxent_model.R
@@ -1,5 +1,15 @@
-## 4B: this approach uses the Maxent algorithm for ENM
-  # NOTE: you need only "lon", and "lat" variables; edit the complete bat data.frame to have
+###
+# this approach uses the Maxent algorithm to produce ENMs
+# this tutorial step assumes that species and environmental data are already available (see steps 1–3)
+
+### 
+# NOTE: the maxent.jar program MUST be within the java directory of the package dismo
+##       to verify/get the directory of dismo use: 
+system.file("java", package = "dismo")
+##       then copy & paste the maxent.jar file into the directory listed by the pathname
+##       download the latest copy of maxent here: https://biodiversityinformatics.amnh.org/open_source/maxent/
+
+
 
 # fire up specific libraries (if not continuing from above)
 library(raster)
@@ -10,78 +20,63 @@ library(ENMeval)
 library(tidyverse)
 
 
-# set working directory. all outputs will be here.
-setwd("~/MaxEnt_Test/")
+# create working directories to organize your data
+getwd()
+dir.create("./MaxEnt")
+dir.create("./Outputs")
 
-# 4B.1. script to get the Feature Classes from Maxent
+
+# get & wrangle species & environmental data
+# species records
+sp <- read.csv(file = "summerREU/sp2.csv", header = T)
+summary(sp)
+# convert dataset to xy
+xy <- sp[, c(2:3)]
+
+
+## NOTE: environmental data must be a raster stack. all environmental data must have the same extent & resolution
+  # create a list of all files within the directory of climate data
+    # choose pattern = ".bil" or ".tif" depending on what extension your raster is
+raster_files <- list.files("/Volumes/ANGELO4/ASC_GIS/Layers/Climate/Climate/wc2-2.5m/", full.names = T, pattern = ".tif")
+  # create a raster stack using the list you created
+predictors <- stack(raster_files)
+  #  verify
+plot(predictors$wc2.1_2.5m_bio_1)
+
+  # create regional extent (xmn, xmx, ymn, ymx)
+geo.extent <- extent(-125, -66, 24, 50)
+  # crop predictors
+predictors <- crop(predictors, geo.extent)
+  # verify by plotting one of the predictors
+plot(predictors$wc2.1_2.5m_bio_1)
+
+
+
+# 4B.4. Create Maxent ENMs using default settings
+
+# extract the Feature Classes from Maxent
 getFCs <- function(html) {
   htmlRead <- readLines(html)
   featureTypes <- htmlRead[grep("Feature types", htmlRead)]
   substr(featureTypes, start=21, stop=nchar(featureTypes)-4)
 }
 
-# get & wrangle your climate & species data (### --- used from section 1 & 2 above --- ###)
-    ## NOTE: if you  already downloaded climate data you can just import as a raster stack
-# create a list of all files within the directory of climate data
-  # choose pattern = ".bil" or ".tif" depending on what extension your raster is
-raster_files <- list.files("wc2-5/", full.names = T, pattern = ".bil")
-# create a raster stack using the list you created
-predictors <- stack(raster_files)
-# to verify, use 
-plot(predictors)
-  # then rm() the objects not in use to clean the environment
-  # as long as you have all rasters in the same resolution & extent, you can load them as a stack
-# then you can crop to the extent you desire (xmn, xmx, ymn, ymx)
-geo.extent <- extent(-86, -65, 16, 24)
-# crop predictors
-predictors <- crop(predictors, geo.extent)
-# verify by plotting one of the predictors
-plot(predictors$wc2.1_2.5m_bio_1)
 
-# 4B.2. define spatial projections of sp data & verify
-pfal.spdf <- SpatialPointsDataFrame(coords = pfal2, data = pfal2,
-                               proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
-summary(pfal.spdf)
 
-# 4B.3. simple map to verify localities (you could try the ggplot2 approach)
-data("wrld_simpl")
-plot(pfal.spdf, pch = 21, cex = 1, col = "red")
-plot(wrld_simpl, add = T)
-
-# 4B.4. Create Maxent ENMs using default settings
-  # create a new directory to save outputs
-dir.create("./Mxnt_Dflt_Out")  ### sipped!
-    #### NEW #####
 ## -- multiple model testing in maxent & determine best parameters in ENMeval
 setwd("~/MaxEnt/Outputs/") # make sure you are here
 ## this must be one level up from the directory where maxent.html is
 
 # create the Maxent model
-pfal.mxnt.dflt <- maxent(predictors, pfal.spdf, path = "Mxnt_Dflt_Out/")
-    ## NOTE: the maxent.jar program MUST be within the java directory of the package dismo
-    ##       to verify/get the directory of dismo use: 
-system.file("java", package = "dismo")
-    ##       then move copy & paste the maxent.jar file into the directory listed by the pathname
-    ## more info here: https://rdrr.io/cran/dismo/man/maxent.html
+sp.mxnt.dflt <- maxent(predictors, xy, path = "./")
 
 # create a raster of your Maxent model prediction
-pfal.dflt.dist <- predict(pfal.mxnt.dflt, predictors, progress = 'text')
+sp.dflt.dist <- predict(sp.mxnt.dflt, predictors, progress = 'text')
 # examine the predicted distribution
-plot(pfal.dflt.dist)
+plot(sp.dflt.dist)
 
-# test the prediction accuracy 
+  # test the prediction accuracy 
   # note that in Maxent.html results you can find the AUC value. to test the predictive accuracy of the model
   # use an independent measure for presence-only data: Boyce Index (Hizrel et al 2006) as implemented in Ecospat
 library(ecospat)
-ecospat.boyce(pfal.dflt.dist, pfal2, window.w = "default", res = 100, PEplot = T)
-  # note that you need to use the species point xy data.frame (here pfal2) not the spdf data used for modeling
-  # your statistic is the Spearman.cor value (= 0.904)
-  # goes between -1 to +1 
-    # positive vlaues = a model which present predictions are consistent with the distribution of presences in the evaluation dataset
-    # values near zero = the model is not different from a random model
-    # negative values = negative values indicate counter predictions, i.e., predicting poor quality areas where presences are more frequent
-  # you can save your raster layer & points file to recalculate this any time
-write.csv(pfal2, file = "Mxnt_Dflt_Out/Pfal_xy.csv", row.names = F) # write sp data
-writeRaster(pfal.dflt.dist, file = "Mxnt_Dflt_Out/Pfal_mxntDist.raster")
-   # to read a raster back 
-      # data.raster <- raster("DIRECTORY/RasterLayerNAME.grd")
+ecospat.boyce(sp.dflt.dist, xy, window.w = "default", res = 100, PEplot = T)
