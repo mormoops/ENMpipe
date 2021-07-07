3434b4e1a51c62f42e1d9dbbd85cd6b77103b329
Angelo <42555726+mormoops@users.noreply.github.com>
2021-07-07 11:43:01 -0400

    Update 08_space_time_project.R

--- tutorial/08_space_time_project.R
+++ tutorial/08_space_time_project.R
@@ -7,3 +7,34 @@
 
 ### 
 
+###
+  ## upload and wrangle bioclim data predictors for the desired time period
+  ## time-scaled climate projections can be obtained from: www.worlclim.org or www.paleoclim.org
+
+library(raster)
+
+  # import rasters from specific directory
+raster_past <- list.files("~/PATH/NAME/TO_PAST_DATA/", full.names = T, pattern = ".tif")
+    # the pattern argument can help find different types of rasters (e.g. *.tif, *.bil, etc.) depending on the file format
+    # NOTE: because this is a pattern matching, make sure to erase the *bil.zip file from the earlier download
+
+  # create a raster stack of bioclim predictors
+predictors_past <- stack(raster_past)
+
+  # examine the uploaded raster files
+plot(predictors_past$RASTER-NAME)
+    # the raster name may vary depending on the raster data names
+
+  # create a geographic extent for your study area
+    # must be equal extent to the present clim data
+    # change lon/lat coordinates (xmin, xmax, ymin, ymax)
+geo.extent <- extent(-86, -65, 16, 27) # e.g. Greater Antilles
+
+  # crop bioclim predictors to the geographic extent
+predictors_past <- crop(predictors_past, geo.extent)
+
+  # examine cropped raster files
+plot(predictors_past$RASTER-NAME)
+
+  ## NOTE: this section is repetitive and can be used to get past or present climate projections OR using present data with a different spatial extent
+
