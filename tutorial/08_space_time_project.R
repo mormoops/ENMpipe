00ef0e75c70a0f16c87c00af28186a4e36ed9784
Angelo <42555726+mormoops@users.noreply.github.com>
2021-07-07 11:33:36 -0400

    Update 08_space_time_project.R

--- tutorial/08_space_time_project.R
+++ tutorial/08_space_time_project.R
@@ -1,1 +1,9 @@
 ######## currently working on...
+
+###
+
+# this section is meant to be run right after section 06 or 07
+# a maxent model with species specific parameter tuning is needed to produce space or time model projections
+
+### 
+
